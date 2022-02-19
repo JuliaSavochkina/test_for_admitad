cd60fefd61d86758d86d933e69d5cc5569e1fd58
JuliaSavochkina <Julie-Webster@ya.ru>
2022-02-19 11:58:16 +0300

    added empty funcs to generate_stat.py

---
+++ usecases/generate_stat.py
@@ -0,0 +1,9 @@
+def get_ours_stat(date_from, date_to):
+    """
+    Выбирает из таблицы с заказами все, принадлежащие нашему источнику и
+    удовлетворяющие указанным датам
+    :param date_from:
+    :param date_to:
+    :return:
+    """
+    pass
