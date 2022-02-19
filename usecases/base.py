8d339309a0be465a70c659b7f0009d516eda0d6f
JuliaSavochkina <Julie-Webster@ya.ru>
2022-02-19 17:52:36 +0300

    updated BaseUseCase

--- usecases/base.py
+++ usecases/base.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
+from typing import Optional
 
 
 class BaseUseCase(ABC):
     @abstractmethod
-    def execute(self, data: dict) -> None:
+    def execute(self, data: dict) -> Optional[dict]:
         pass
