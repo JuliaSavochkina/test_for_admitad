74c41f97aae0c3a0898ea49f39574afb6e7d0913
JuliaSavochkina <Julie-Webster@ya.ru>
2022-02-19 11:56:40 +0300

    added table for LastClick

--- entities/click_log_entity.py
+++ entities/click_log_entity.py
@@ -1,4 +1,18 @@
-class ClickLog:
+from sqlalchemy import Column, String, Integer
+
+from servises.create_db import Base
+
+
+class LastClick(Base):
+    __tablename__ = 'last_click'
+
+    id = Column(Integer, primary_key=True)
+    client_id = Column(String(10))
+    user_agent = Column(String)
+    location = Column(String)
+    referer = Column(String)
+    date = Column(String(30))
+
     def __init__(
             self,
             client_id: str,
