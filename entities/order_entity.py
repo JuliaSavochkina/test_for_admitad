5e0072c6422eecaf3900c0fa953e296c2dadddb4
JuliaSavochkina <Julie-Webster@ya.ru>
2022-02-19 11:56:47 +0300

    added table for Order

--- entities/order_entity.py
+++ entities/order_entity.py
@@ -1,4 +1,16 @@
-class Order:
+from sqlalchemy import Column, String, Integer
+
+from servises.create_db import Base
+
+
+class Order(Base):
+    __tablename__ = 'orders'
+
+    id = Column(Integer, primary_key=True)
+    client_id = Column(String(10))
+    date = Column(String(30))
+    source = Column(String)
+
     def __init__(
             self,
             date: str,
