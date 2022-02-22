1a25547cbf73e2d1a499b9884745aa86867b9964
JuliaSavochkina <Julie-Webster@ya.ru>
2022-02-22 16:34:51 +0300

    removed UpdateClickLogUseCase

--- usecases/update_db.py
+++ usecases/update_db.py
@@ -7,32 +7,6 @@ from services import datasource
 from usecases.base import BaseUseCase
 
 
-class UpdateClickLogUseCase(BaseUseCase):
-    def execute(self, data: dict) -> None:
-        """
-        В рамках метода осуществляется поиск записи по client_id (такая должна быть одна),
-        все столбцы записи, кроме client_id обновляются в соответсвии с поданными данными
-        :param data: словарь с логом
-        :return:
-        """
-        client_id = data['client_id']
-        try:
-            to_update = update(LastClick).where(LastClick.client_id == client_id). \
-                values(
-                {
-                    'user_agent': data['User-Agent'],
-                    'location': data['document.location'],
-                    'referer': data['document.referer'],
-                    'date': data['date']
-                }
-            )
-        except Exception as e:
-            logging.error(f"Was not able to update this click in DB: because of {e}")
-            datasource.session.rollback()  # не ясно как быть с этим
-        else:
-            datasource.conn.execute(to_update)
-
-
 class UpdateSourceUseCase(BaseUseCase):
     def execute(self, data: dict) -> None:
         """
