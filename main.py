c8448ebdcfdcd108311d1028235087607f1b964d
JuliaSavochkina <Julie-Webster@ya.ru>
2022-02-23 15:36:55 +0300

    added verification for add_log

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from typing import List
 
 from flask import Flask, request, jsonify
@@ -5,7 +6,9 @@ from flask import Flask, request, jsonify
 # импорт для того, чтобы декларативная база знала, какие таблицы создавать
 from entities import Order, User
 from services import datasource
+from usecases.generate_stat import get_statistics
 from usecases.log_analytics import AnalyseLogUseCase
+from usecases.utils import REQUIRED_FIELDS
 
 app = Flask(__name__)
 datasource.create_tables()
@@ -26,13 +29,17 @@ def add_log():
     Эндпоинт для добавления лога в БД
     :return: сообщение об успешности регистрации лога
     """
-    # валидация
     content: List[dict] = request.json
+    error_logs = []
     usecase = AnalyseLogUseCase()
     for log in content:
-        # валидация
+        if Counter(log.keys()) != Counter(REQUIRED_FIELDS):
+            error_logs.append(log)
+            continue
         usecase.execute(log)
-    return jsonify({"status": "Log added to DB"}), 200
+
+    return jsonify({"status": "Log added to DB",
+                    "not processed log(s): ": error_logs}), 200
 
 
 @app.route('/get_stat', methods=['GET'])
