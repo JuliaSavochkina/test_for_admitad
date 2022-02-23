772690e221b9ce5a522b5dccd756976a4ffd9e36
JuliaSavochkina <Julie-Webster@ya.ru>
2022-02-23 11:16:53 +0300

    updated BaseUseCase

--- usecases/base.py
+++ usecases/base.py
@@ -3,6 +3,9 @@ from typing import Optional
 
 
 class BaseUseCase(ABC):
+    """
+    Абстрактный класс для всех юзкейсов
+    """
     @abstractmethod
     def execute(self, data: dict) -> Optional[dict]:
         pass
