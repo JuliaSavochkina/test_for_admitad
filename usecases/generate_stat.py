69ae83884456afdd56e9382234a835181656a516
JuliaSavochkina <Julie-Webster@ya.ru>
2022-02-23 15:35:50 +0300

    added get_statistics

--- usecases/generate_stat.py
+++ usecases/generate_stat.py
@@ -1,4 +1,10 @@
-def get_statistics(date_from, date_to):
+from datetime import datetime
+
+from entities import Order
+from services import datasource
+
+
+def get_statistics(date_from: str, date_to: str):
     """
     Выбирает из таблицы с заказами все, принадлежащие нашему источнику и
     удовлетворяющие указанным датам
@@ -6,4 +12,9 @@ def get_statistics(date_from, date_to):
     :param date_to:
     :return:
     """
-    pass
+    prepared_date_from = datetime.strptime(date_from, '%Y-%m-%d')
+    prepared_date_to = datetime.strptime(date_to, '%Y-%m-%d')
+    amount_of_orders = datasource.session.query(Order).filter(Order.date <= prepared_date_to). \
+        filter(Order.date >= prepared_date_from).filter(Order.source == 'referal.ours.com').count()
+    return amount_of_orders
+
