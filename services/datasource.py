f248bb8372315fbacb0c50ae78dd00024068e120
JuliaSavochkina <Julie-Webster@ya.ru>
2022-02-26 15:54:07 +0300

    removed connection from Datasource

--- services/datasource.py
+++ services/datasource.py
@@ -12,7 +12,6 @@ class Datasource:
     def __init__(self):
         url = f'postgresql://{Config.POSTGRES_USER}:{Config.POSTGRES_PASSWORD}@{Config.HOST}:{Config.PORT}/{Config.POSTGRES_DB}'
         self.engine = create_engine(url)
-        self.conn = self.engine.connect()
         get_session = sessionmaker(bind=self.engine)
         self.session = get_session()
         self.Base = declarative_base()
