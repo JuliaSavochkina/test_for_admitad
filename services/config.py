f0b3d782a7fdf6302a104209dbc927de73efe865
JuliaSavochkina <Julie-Webster@ya.ru>
2022-03-03 08:51:43 +0300

    update Config to getenv

--- services/config.py
+++ services/config.py
@@ -1,9 +1,13 @@
+import os
+
+
 class Config:
     """
     В этом классе сосредоточенны все переменные для запуска проекта
     """
-    HOST = 'db'
-    PORT = '5432'
-    POSTGRES_USER = 'postgres'
-    POSTGRES_PASSWORD = 'postgres'
-    POSTGRES_DB = 'postgres'
+    HOST = os.getenv('HOST')
+    PORT = os.getenv('PORT')
+    POSTGRES_USER = os.getenv('POSTGRES_USER')
+    POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+    POSTGRES_DB = os.getenv('POSTGRES_DB')
+    LOG_LEVEL = int(os.getenv('LOG_LEVEL'))
